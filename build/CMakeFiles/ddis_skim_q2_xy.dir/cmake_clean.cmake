f5c6f5aab84812927ebfc5c59e8d2503495660d0
Hadi Hashamipour <120186729+hashamipour@users.noreply.github.com>
2025-09-17 21:03:40 +0200

    adding the new skimmer with x_Bj and inelasticity

---
+++ build/CMakeFiles/ddis_skim_q2_xy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ddis_skim_q2_xy.dir/analysis/DDIS_Skim_Q2_with_xy.cpp.o"
+  "CMakeFiles/ddis_skim_q2_xy.dir/analysis/DDIS_Skim_Q2_with_xy.cpp.o.d"
+  "ddis_skim_q2_xy"
+  "ddis_skim_q2_xy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ddis_skim_q2_xy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ddis_skim_q2_xy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ddis_skim_q2_xy.
+# This may be replaced when dependencies are built.
